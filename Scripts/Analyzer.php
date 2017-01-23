6e3c6c886a753dc877245c5a5fe8608f4fd6e93e
Terentyev Alexey <alexey@terentyev.pro>
2017-01-24 02:04:40 +0600

    #6 Analyzer is Done

--- Scripts/Analyzer.php
+++ Scripts/Analyzer.php
@@ -28,39 +28,29 @@ class Analyzer
 			$data = array ('count' => 1, $key => 1);
 			fwrite($handle, json_encode($data));
 			fclose($handle);
-//			echo 'key='.$key;
-//            var_dump($handle);
-			return 'New file. 100';
+			return '100';
 		}
 
 		$x = 1;
-		$contents = file_get_contents(self::PATCH);
-/*
+
 		$contents = fread($handle, filesize(self::PATCH));
         $contents = json_decode($contents, self::JSON_AS_ARRAY);
-*/
-//        $contents = str_split($contents);
-        var_dump($contents);
-        echo PHP_EOL.'---------'.PHP_EOL;
 
         if (isset($contents[$key])) {
-            echo $contents[$key];
-			$x = intval($contents[$key]);
+			$contents[$key]++;
 		} else {
-            echo 'Nope '.$key.PHP_EOL;
 			$contents[$key] = 1;
 		};
 
-		$contents['count'] = intval($contents['count']) + 1;
+		$contents['count']++;
 		
 		$proc = ($contents[$key] / $contents['count'] * 100);
 
         rewind($handle);
 		fwrite($handle, json_encode($contents));
 		fclose($handle);
-
-//        echo 'key='.$key.' ';
-		return $proc;
+		
+		return (string) $proc;
 	}
 	
 	/**
